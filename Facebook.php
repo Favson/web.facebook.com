133b31213c628e93a336a81a6a136673f2f9ee6f
Adebisi Favour <adebisimomoluwa@gmail.com>
2024-08-28 15:33:24 +0100

    facebook clone updated

--- Facebook.php
+++ Facebook.php
@@ -8,6 +8,9 @@
     <title>Facebook-log in or sign up</title>
     <link rel="stylesheet" href="bootstrap-5.3.2-dist/bootstrap-5.3.2-dist/css/bootstrap.css">
     <style>
+        .facebookText{
+            width: 100%;
+        }
         form{
             border: 1px solid white;
             border-radius: 10px;
@@ -20,17 +23,47 @@
         input:focus{
             outline: 2px solid rgb(127, 181, 228);
         }
+        .wholeDiv{
+            margin-top: 140px;
+        }
+        .facebook{
+            width: 75%;
+        }
+
+        .facebookdiv{
+            margin-top: 30px;
+        }
+
+        .formDiv{
+            width: 38%;
+        }
+        @media(max-width:476px){
+            .wholeDiv{
+                margin-top:0px;
+            }   
+            .facebook{
+                margin: 0 auto;
+            }
+            .formDiv{
+                width: 100%;
+            }
+
+            .facebookText{
+                width: 100%;
+            }
+        }
+         
     </style>
 </head>
 
 <body style="background-color: rgb(211, 218, 226);">
-    <div class="row w-100 mx-auto">
-        <div class="col-lg-6 col-md-6 col-sm-12">
-            <img src="fascebook.svg" alt="" class="w-50">
-            <h3 style="width: 80%;">Facebook helps you connect and share with the people in your life.</h3>
+    <div class="container row w-100 mx-auto wholeDiv">
+        <div class="col-lg-6 col-md-6 col-sm-12 facebookdiv">
+            <img src="fascebook.svg" alt="" class="facebook">
+            <h3 class="facebookText ms-4">Facebook helps you connect and share with the people in your life.</h3>
         </div>
-        <div class="col-lg-4 col-md-6 col-sm-12 text-center">
-            <form method="post" class="bg-white py-3 shadow-lg w-100 text-center" action="Facebook.html">
+        <div class="col-lg-4 col-md-6 col-sm-12 text-center formDiv">
+            <form method="post" class="bg-white py-4 shadow-lg w-100 text-center" action="Facebook.html">
                 <input type="email or number" placeholder="Email address or Phone Number" width="100px" class="col-11 border-tertiary mb-3">
                 <input type="password" placeholder="Password" required class="col-11 border-tertiary mb-3"><br>  
                 <button class="bg-primary col-11 text-light btn btn-md fw-bolder fs-5">Log in</button>
